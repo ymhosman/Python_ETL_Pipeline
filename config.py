0f1c2403c85417464bf8b97c4e4aad571c7e92c2
youssefmho <el21ymho@leeds.ac.uk>
2026-04-14 11:27:52 +0200

    cleaning sources

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
-INPUT_FILE = "data_input/order_details.csv"
-REF_FILE = "data_input/restaurant_details.csv"
-BASE_OUTPUT = "data_output"
-USER = "username"
-PASS = "password"
-URL = "sqlite:///etl.db"
-TABLE_NAME = "processed_data"
+import os
+from dotenv import load_dotenv
+
+# Load variables from .env into environment
+load_dotenv()
+
+# Read values
+INPUT_FILE = os.getenv("INPUT_FILE")
+REF_FILE = os.getenv("REF_FILE")
+BASE_OUTPUT = os.getenv("BASE_OUTPUT", "data_output")
+
+DB_URL = os.getenv("DB_URL")
+TABLE_NAME = os.getenv("TABLE_NAME")
+
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
