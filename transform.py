e29dc9c292814bd4ce616761d56361f33f10bffc
youssefmho <el21ymho@leeds.ac.uk>
2026-04-14 02:13:36 +0200

    fixed aggregation and missing restaurant reference

--- transform.py
+++ transform.py
@@ -1,18 +1,22 @@
 import pandas as pd
 import numpy as np
 
-db_clean = pd.DataFrame()
-db_rejected = pd.DataFrame()
 def transform(orders_raw: pd.DataFrame, ref_raw: pd.DataFrame):
     # Transform
     # Clean null customer ratings to default to 2.5 (half of max 5)
     orders_raw['customer_rating'] = orders_raw['customer_rating'].fillna(2.5)
 
     # inner join with reference table
-    db = pd.merge(orders_raw, ref_raw, on='restaurant_code', how='inner')
+    db = pd.merge(orders_raw, ref_raw, on='restaurant_code', how='left')
 
-    # Encode 'own_driver' column to numeric
-    db["own_driver"] = db["own_driver"].map({"No" : 0, "Yes" : 1})
+    # Encode own driver
+    db["own_driver"] = db["own_driver"].map({"No": 0, "Yes": 1})
+
+    
+    db["own_driver"] = db["own_driver"].fillna(0)
+    db["delivery_fee"] = db["delivery_fee"].fillna(40)
+    db["discount_applied"] = db["discount_applied"].fillna(0)
+    db["adv_delivery_time"] = db["adv_delivery_time"].fillna(40)
 
     # Derive delivered on time field
     db["delivered_on_time"] = np.where(db['time_to_deliver'] > db['adv_delivery_time'], "No", "Yes")
@@ -25,7 +29,7 @@ def transform(orders_raw: pd.DataFrame, ref_raw: pd.DataFrame):
     # Soft error 1: order price higher than 3000
     db.loc[db['order_price'] > 3000, 'data_quality_flag'] += 'HIGH PRICE ALERT,'
     # Soft error 2: restaurant category unknown
-    db.loc[db['category'].isnull(), 'data_quality_flag'] += 'UNKNOWN RESTAURANT CATEGORY,'
+    db.loc[db['category'].isnull(), 'data_quality_flag'] += 'MISSING RESTAURANT REFERENCE,'
     db['data_quality_flag'] = db['data_quality_flag'].str.rstrip(',')
     db['category'] = db['category'].fillna("Unknown")
     db.loc[db['data_quality_flag'] == '', 'data_quality_flag'] = 'OK'
@@ -50,7 +54,7 @@ def reject(db: pd.DataFrame):
     db_rejected = db[db['reject_reason'] != '']
     db_clean = db[db['reject_reason'] == '']
 
-    db_rejected = db_rejected.drop(columns=['time_to_deliver', 'discount_applied', 'own_driver', 'adv_delivery_time'])
+    #db_rejected = db_rejected.drop(columns=['time_to_deliver', 'discount_applied', 'own_driver', 'adv_delivery_time'])
     db_clean = db_clean.drop(columns=['order_price', 'time_to_deliver', 'discount_applied', 'own_driver', 'adv_delivery_time', 'reject_reason'])
     return db_clean, db_rejected
 
